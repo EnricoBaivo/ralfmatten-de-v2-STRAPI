daa13a7fdadb51b21622363760e9157e921439ea
Enrico Bono <enrico.bono.home@gmail.com>
2023-08-15 19:02:35 +0200

    init

---
+++ src/api/hologramm-praesentation/controllers/hologramm-praesentation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hologramm-praesentation controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hologramm-praesentation.hologramm-praesentation');
---
+++ src/api/hologramm-praesentation/routes/hologramm-praesentation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hologramm-praesentation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hologramm-praesentation.hologramm-praesentation');
